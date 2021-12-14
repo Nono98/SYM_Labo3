afb925d0b10b30562e75a6722422a8c5d3a86137
Thierry <adrien.peguiron@heig-vd.ch>
2021-12-14 19:49:13 +0100

    NFC fonctionnel
    le NFC fonctionne, manque à répondre aux questions et cleanup le code

--- app/src/main/java/ch/heigvd/iict/sym/lab/lab3/NFCActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/lab/lab3/NFCActivity.kt
@@ -12,11 +12,25 @@ import android.content.IntentFilter
 import android.app.PendingIntent
 import android.content.IntentFilter.MalformedMimeTypeException
 import android.util.Log
+import android.os.CountDownTimer
+import android.widget.Button
 
 
 class NFCActivity : AppCompatActivity() {
 
+    private lateinit var loginButton : Button
+    private lateinit var highButton : Button
+    private lateinit var mediumButton : Button
+    private lateinit var lowButton : Button
+    private lateinit var username: TextView
+    private lateinit var password: TextView
+
+    private var authenticated = false
     val TAG = "NfcDemo"
+    private final val AUTHENTICATE_MAX = 15
+    private final val AUTHENTICATE_MEDIUM = 10
+    private final val AUTHENTICATE_LOW = 5
+    private var authenticateCurrent = 0
 
     private var mTextView: TextView? = null
     private var mNfcAdapter: NfcAdapter? = null
@@ -24,7 +38,28 @@ class NFCActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfcactivity)
 
-        mTextView = findViewById(R.id.textView_explanation);
+        loginButton = findViewById(R.id.nfc_btnLogin)
+        highButton = findViewById(R.id.nfc_btnHigh)
+        mediumButton = findViewById(R.id.nfc_btnMid)
+        lowButton = findViewById(R.id.nfc_btnLow)
+        username = findViewById(R.id.nfc_username)
+        password = findViewById(R.id.nfc_password)
+
+        loginButton.setOnClickListener{
+            login(username.text.toString(), password.text.toString())
+        }
+
+        highButton.setOnClickListener {
+            nfcLogin(AUTHENTICATE_MAX)
+        }
+
+        mediumButton.setOnClickListener {
+            nfcLogin(AUTHENTICATE_MEDIUM)
+        }
+
+        lowButton.setOnClickListener {
+            nfcLogin(AUTHENTICATE_LOW)
+        }
 
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
@@ -38,14 +73,33 @@ class NFCActivity : AppCompatActivity() {
         }
 
 
-        handleIntent(getIntent());
+        handleIntent(intent);
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
     }
 
     private fun handleIntent(intent: Intent) {
+        //source : https://www.andreasjakl.com/nfc-tags-ndef-and-android-with-kotlin/
         if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
-                // Process the messages array.
+            val rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            val ndefMsg = rawMessages?.get(0) as NdefMessage
+            val ndefRecord = ndefMsg.records[0]
+            if (String(ndefRecord.payload).contains("test")) {
+                authenticateCurrent = AUTHENTICATE_MAX + 1
+                //source : https://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
+                //on perd 1 niveau d'authentification toute les 10 secondes
+                object : CountDownTimer((AUTHENTICATE_MAX * 10000).toLong(), 10000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        authenticateCurrent--
+                    }
+
+                    override fun onFinish() {
+                        authenticateCurrent = 0
+                    }
+                }.start()
             }
         }
     }
@@ -76,11 +130,31 @@ class NFCActivity : AppCompatActivity() {
 
     override fun onPause(){
         super.onPause()
-        stopForegroundDispatch();
+        stopForegroundDispatch()
     }
+
     private fun stopForegroundDispatch() {
         if (mNfcAdapter != null) mNfcAdapter!!.disableForegroundDispatch(this)
     }
 
+    private fun login(username: String, password: String){
+        if (username=="user1" && password=="mdpUser1!"){
+            authenticated = true
+            Toast.makeText(this, "login reussi", Toast.LENGTH_LONG).show()
+        }
+        else {
+            Toast.makeText(this, "echec du login", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun nfcLogin(authenticationLevel : Int): Boolean {
+        if ((authenticateCurrent >= authenticationLevel) && authenticated){
+            Toast.makeText(this, "acces accorde", Toast.LENGTH_LONG).show()
+            return true
+        }
+        Toast.makeText(this, "acces refuse", Toast.LENGTH_LONG).show()
+
+        return false
+    }
 
 }
