58526b52975256792039fff6d7d0e7a12c894bd3
Thierry <adrien.peguiron@heig-vd.ch>
2021-12-18 16:53:28 +0100

    commentaires du code et réponse aux questions NFC

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # SYM_Labo3
+
+### 2. Balises NFC
+
+#### 2.2 Manipulation
+
+##### Choix d'implémentation :
+
+Avant de tenter d'accéder aux différentes ressources, l'utilisateur devra se connecter à l'aide d'une paire `username` `password`. Dans le cadre de ce laboratoire, cette paire est codée en dur dans le code. C'est une pratique à éviter en temps normal mais le but de cette manipulation était de se familiarisé avec les balises NFC et le login a été mis en second plan. 
+
+`username` : `user1`
+
+`password` : `mdpUser1!`
+
+Une fois l'utilisateur connecté, il doit scanner une balise NFC avant de pouvoir utiliser les différents boutons.
+
+Lors du scan, l'application vérifie si le premier message NFC contient la chaîne de caractères `test`.
+
+Si c'est le cas, le niveau d'authentification de l'utilisateur est défini à 15. Toutes les 10 secondes, il perd 1 niveau d'authentification. 
+
+Les 3 boutons à disposition son accessibles en fonction du niveau d'authentification. Le premier nécessite au moins 15, le deuxième 10 et le dernier 5. 
+
+Si l'utilisateur scanne à nouveau la balise NFC, son niveau d'authentification remonte à 15.
+
+Des messages sous forme de `toast` sont affichés pour indiquer la réussite ou échec du login et de l'accès aux boutons.
+
+#### 2.4 Question
+
+##### 2.4.1 
+
+> Dans la manipulation ci-dessus, les tags NFC utilisés contiennent 4 valeurs textuelles codées en UTF-8 dans un format de message NDEF. Une personne malveillante ayant accès au porte-clés peut aisément copier les valeurs stockées dans celui-ci et les répliquer sur une autre puce NFC.
+>
+> A partir de l’API Android concernant les tags NFC3, pouvez-vous imaginer une autre approche pour rendre plus compliqué le clonage des tags NFC? Existe-il des limitations? Voyez-vous d’autres possibilités?
+
+La présence d'authentification à multiples facteurs donne à l'application une certaine sécurité même si les valeurs de la puce NFC ont été clonées. En effet, l'utilisateur malveillant doit également connaître la paire `username` `mot de passe`, ou avoir accès à une application où le login a déjà été effectué.
+
+La plupart des tags ont un id unique non modifiable. Celui-ci pourrait être utilisé pour spécifié quel tag contient les données et éviter qu'un autre tag contenant des données clonées fonctionnent.
+
+Mais certains tag ont une id modifiable ce qui permettrait à l'attaquant d'avoir le même id de tag que l'original et contourner cette protection.
+
+Les tags NFCs ne sont pas adaptés à l'authentification de part leur facilité à être clonés, il est préférable d'utiliser d'autres moyens plus sécurisés.
+
+Il existe également des smartcards sans contact qui fonctionnent de manière similaire au NFC et permettent d'accéder aux données à l'aide d'une clé asymétrique. Cette manière de procéder donne une couche de sécurité supplémentaire non négligeable.
+
+##### 2.4.2
+
+> Est-ce qu’une solution basée sur la vérification de la présence d’un iBeacon sur l’utilisateur, par exemple sous la forme d’un porte-clés serait préférable? Veuillez en discuter.
+
+Les tags NFC peuvent facilement être clonés mais cela nécessite d'avoir un accès direct à la puce NFC car la portée de celles-ci est très faible. Les iBeacon ayant une portée bien plus grande, le clonage est alors grandement facilité sans avoir besoin d'un accès physique direct.
+
+Il serait également possible de s'authentifier avec l'iBeacon d'une personne se trouvant à proximité sans que celle-ci ne réalise.
--- app/src/main/java/ch/heigvd/iict/sym/lab/lab3/NFCActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/lab/lab3/NFCActivity.kt
@@ -1,3 +1,16 @@
+/*
+Auteurs : Peguiron Adrien, Placherel Noémie, Viotti Nicolas
+
+Cette activité permet à un utilisateur d'accèder à différents boutons après une authentification.
+L'authentification se fait en deux parties, il se connecte d'abord à l'aide de credentials, et il doit
+ensuite scanner une puce NFC pour prouver son identité.
+
+3 niveaux d'authentification sont implémentés. Le but étant que l'utilisateur perde des privilièges si
+il reste authentifié trop longtemps. Pour les regagner, il suffit de scanner à nouveau la puce NFC.
+En cas de vol de téléphone, les données confidentielles ne pourraient donc pas être accédées par le voleur
+(sauf dans le cas ou il aurait également volé la puce NFC)
+ */
+
 package ch.heigvd.iict.sym.lab.lab3
 
 import android.content.Intent
@@ -25,15 +38,20 @@ class NFCActivity : AppCompatActivity() {
     private lateinit var username: TextView
     private lateinit var password: TextView
 
+    // indique si l'utilisateur s'est loggé avec la paire username/password
     private var authenticated = false
+
     val TAG = "NfcDemo"
+
+    // Les différents niveau d'authentification
     private final val AUTHENTICATE_MAX = 15
     private final val AUTHENTICATE_MEDIUM = 10
     private final val AUTHENTICATE_LOW = 5
+    // Le niveau d'authentification actuel
     private var authenticateCurrent = 0
 
-    private var mTextView: TextView? = null
     private var mNfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfcactivity)
@@ -64,12 +82,12 @@ class NFCActivity : AppCompatActivity() {
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         if (mNfcAdapter == null) {
-            // Stop here, we definitely need NFC
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show()
+            // Si le NFC n'est pas supporté, on s'arrête
+            Toast.makeText(this, "Cet appareil ne supporte pas le NFC", Toast.LENGTH_LONG).show()
             finish();
             return;
         } else if(!mNfcAdapter!!.isEnabled()) {
-            Toast.makeText(this, "NFC is disabled", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "le NFC est désactivé", Toast.LENGTH_LONG).show()
         }
 
 
@@ -84,13 +102,15 @@ class NFCActivity : AppCompatActivity() {
     private fun handleIntent(intent: Intent) {
         //source : https://www.andreasjakl.com/nfc-tags-ndef-and-android-with-kotlin/
         if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            // Récupération du premier message NFC
             val rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
             val ndefMsg = rawMessages?.get(0) as NdefMessage
             val ndefRecord = ndefMsg.records[0]
             if (String(ndefRecord.payload).contains("test")) {
+                // Si le premier messaeg contient "test", le niveau d'authentification est alors au maximum
                 authenticateCurrent = AUTHENTICATE_MAX + 1
                 //source : https://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
-                //on perd 1 niveau d'authentification toute les 10 secondes
+                // On perd 1 niveau d'authentification toute les 10 secondes
                 object : CountDownTimer((AUTHENTICATE_MAX * 10000).toLong(), 10000) {
                     override fun onTick(millisUntilFinished: Long) {
                         authenticateCurrent--
@@ -137,22 +157,23 @@ class NFCActivity : AppCompatActivity() {
         if (mNfcAdapter != null) mNfcAdapter!!.disableForegroundDispatch(this)
     }
 
+
     private fun login(username: String, password: String){
         if (username=="user1" && password=="mdpUser1!"){
             authenticated = true
-            Toast.makeText(this, "login reussi", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Login réussi", Toast.LENGTH_LONG).show()
         }
         else {
-            Toast.makeText(this, "echec du login", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Echec du login", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun nfcLogin(authenticationLevel : Int): Boolean {
         if ((authenticateCurrent >= authenticationLevel) && authenticated){
-            Toast.makeText(this, "acces accorde", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Accès accordé", Toast.LENGTH_LONG).show()
             return true
         }
-        Toast.makeText(this, "acces refuse", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Accès refusé", Toast.LENGTH_LONG).show()
 
         return false
     }
