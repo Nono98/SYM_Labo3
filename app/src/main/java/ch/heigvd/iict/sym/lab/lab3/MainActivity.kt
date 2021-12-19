59fd2d7b95e0ec71a1b2aaa6df12854d26e0862e
Nono98 <noemie.plancherel@heig-vd.ch>
2021-12-19 18:10:17 +0100

    Rfactor

--- app/src/main/java/ch/heigvd/iict/sym/lab/lab3/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/lab/lab3/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import ch.heigvd.iict.sym.lab.lab3.ibeacon.iBeaconActivity
+import ch.heigvd.iict.sym.lab.lab3.ibeacon.IBeaconActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var barcodeButton : Button
